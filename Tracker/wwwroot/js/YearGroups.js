f4995529c1e62765885a3776e5db881a8a001f48
Dan <dan@ebsllp.com>
2019-11-24 21:23:59 +0000

    * select term

--- Tracker/wwwroot/js/YearGroups.js
+++ Tracker/wwwroot/js/YearGroups.js
@@ -4,7 +4,8 @@ let subjectId = 0;
 let classId = 0;
 let subject = '';
 let objectives = [];
-
+let assessTwice = 0;
+let TermCode = ''
 $(function () {
     yearGroups.loadYearGroups();
     yearGroups.loadObjectives();
@@ -22,7 +23,7 @@ var yearGroups = {
 
             $("#yearGroups tbody").html("");
             while (i < l) {
-                $("#yearGroups tbody").append("<tr><td><input type='button' id='" + result[i].oid + "' code='" + result[i].code + "' onclick='yearGroups.loadSubjects(this)' value='Year " + result[i].code + "' class='yearGroupsButton btn btn-primary selectButtons' /></td></tr>");
+                $("#yearGroups tbody").append("<tr><td><input type='button' assessTwice='" + result[i].assessTwicePerTerm + "' id='" + result[i].oid + "' code='" + result[i].code + "' onclick='yearGroups.loadSubjects(this)' value='Year " + result[i].code + "' class='yearGroupsButton btn btn-primary selectButtons' /></td></tr>");
                 i++;
             }
             $(
@@ -30,9 +31,11 @@ var yearGroups = {
         });
     },
     loadSubjects: function (row) {
-        yearGroupId = $(row).attr("id");
-        yearGroupCode = $(row).attr("code");
-
+        if (row != undefined) {
+            yearGroupId = $(row).attr("id");
+            yearGroupCode = $(row).attr("code");
+            assessTwice = $(row).attr("assessTwice");
+        }
         $.ajax({
             url: '/YearGroups?handler=SubjectsList',
             data: {
@@ -45,18 +48,46 @@ var yearGroups = {
             $("#yearGroupContainer").hide();
             $("#subjects tbody").html("");
             while (i < l) {
-                $("#subjects tbody").append("<tr><td><input type='button' id='" + result[i].subjectId + "' code='" + result[i].displayName  + "' onclick='yearGroups.loadPupils(this)' value='" + result[i].displayName + "' class='subjectButtons btn btn-primary selectButtons' /></td></tr>");
+                $("#subjects tbody").append("<tr><td><input type='button' id='" + result[i].subjectId + "' code='" + result[i].displayName + "' onclick='yearGroups.loadTerms(this)' value='" + result[i].displayName + "' class='subjectButtons btn btn-primary selectButtons' /></td></tr>");
                 i++;
             }
             $("#subjects tbody").append("<tr><td><input type='button' value='Back' onclick='yearGroups.loadYearGroups()' class='subjectButtons btn btn-warning selectButtons' /></td></tr>");
             $("#subjectContainer").show();
         });
     },
-    loadPupils: function (row) {
+    loadTerms: function (row) {
         subjectId = $(row).attr("id");
         subject = $(row).attr("code");
 
-        $("#pupil-title").html('Year Group: ' + yearGroupCode + ', Subject: ' + subject);
+        $("#subjectContainer").hide();
+        $("#terms tbody").html("");
+
+        $("#terms tbody").append("<tr><td><input type='button' id='termAutumn' code='AUT' onclick='yearGroups.loadPupils(this)' value='Autumn' class='termButtons btn btn-primary selectButtons' /></td></tr>");
+        if (assessTwice > 0) {
+            $("#terms tbody").append("<tr><td><input type='button' id='termAutumnHalf' code='AUT2' onclick='yearGroups.loadPupils(this)' value='Autumn Half' class='termButtons btn btn-primary selectButtons' /></td></tr>");
+        }
+
+        $("#terms tbody").append("<tr><td><input type='button' id='termSpring' code='SPR' onclick='yearGroups.loadPupils(this)' value='Spring' class='termButtons btn btn-primary selectButtons' /></td></tr>");
+        if (assessTwice > 0) {
+            $("#terms tbody").append("<tr><td><input type='button' id='termSpringHalf' code='SPR2' onclick='yearGroups.loadPupils(this)' value='Spring Half' class='termButtons btn btn-primary selectButtons' /></td></tr>");
+        }
+
+        $("#terms tbody").append("<tr><td><input type='button' id='termSummer' code='SUM' onclick='yearGroups.loadPupils(this)' value='Summer' class='termButtons btn btn-primary selectButtons' /></td></tr>");
+        if (assessTwice > 0) {
+            $("#terms tbody").append("<tr><td><input type='button' id='termSummerHalf' code='SUM2' onclick='yearGroups.loadPupils(this)' value='Summer Half' class='termButtons btn btn-primary selectButtons' /></td></tr>");
+        }
+
+        $("#terms tbody").append("<tr><td><input type='button' id='termTarget' code='TAR' onclick='yearGroups.loadPupils(this)' value='Target' class='termButtons btn btn-primary selectButtons' /></td></tr>");
+        $("#terms tbody").append("<tr><td><input type='button' id='termBaseline' code='BAS' onclick='yearGroups.loadPupils(this)' value='Baseline' class='termButtons btn btn-primary selectButtons' /></td></tr>");
+
+        $("#terms tbody").append("<tr><td><input type='button' value='Back' onclick='yearGroups.loadSubjects()' class='termButtons btn btn-warning selectButtons' /></td></tr>");
+        $("#termContainer").show();
+    },
+    loadPupils: function (row) {
+        termCode = $(row).attr("code");
+        termName = $(row).val();
+
+        $("#pupil-title").html('Year Group: ' + yearGroupCode + ', Subject: ' + subject + ', Term: ' + termName);
         $.ajax({
             url: '/YearGroups?handler=ClassId',
             data: {
@@ -76,7 +107,38 @@ var yearGroups = {
                 $('.pupilTable').bootstrapTable({
                     data: data
                 });
-                $("#subjectContainer").hide();
+                $("#termContainer").hide();
+
+                switch (termCode) {
+                    case 'AUT' : {
+                        $('.AUT').removeClass('hide-col');
+                        break;
+                    }
+                    case 'AUT2': {
+                        $('.AUT2').removeClass('hide-col');
+                        break;
+                    }
+                    case 'SPR': {
+                        $('.SPR').removeClass('hide-col');
+                        break;
+                    }
+                    case 'SPR2': {
+                        $('.SPR2').removeClass('hide-col');
+                        break;
+                    }
+                    case 'SUM': {
+                        $('.SUM').removeClass('hide-col');
+                        break;
+                    }
+                    case 'SUM': {
+                        $('.SUM').removeClass('hide-col');
+                        break;
+                    }
+                    case 'SUM2': {
+                        $('.SUM2').removeClass('hide-col');
+                        break;
+                    }
+                }
                 $("#pupilsContainer").show();
             });
         });
